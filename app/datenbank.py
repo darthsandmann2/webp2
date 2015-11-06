ed3361df0b284d7d16850577c0080e3a0db1adb6
Darth Sandmann <kai-wans@gmx.de>
2015-11-06 13:58:20 +0100

    datenbank update

--- app/datenbank.py
+++ app/datenbank.py
@@ -15,20 +15,59 @@ class datenbank_db(object):
 		self.lesen_user_db()
 ## --------------------------------------------------------------------##
 
-
-
-## --------------------------------------------------------------------##
-## Json Dateien einlesen
-## --------------------------------------------------------------------##
-	def lesen_content_db(self):
-		ordner = os.listdir('data')
-		datei = codecs.open(os.path.join('data', 'content.json'), 'rU', 'utf-8')
-		dateiinhalt = datei.read()
-		self.content = json.loads(dateiinhalt)
+	
 		
-	def lesen_user_db(self):
-		ordner = os.listdir('data')
-		datei = codecs.open(os.path.join('data', 'user.json'), 'rU', 'utf-8')
-		dateiinhalt = datei.read()
-		self.content = json.loads(dateiinhalt)
 ## --------------------------------------------------------------------##
+## Verzeichnisverwaltung
+## --------------------------------------------------------------------##
+	def erstellen_themavz_db(self, thema):
+		mkdir(verzeichnis)
+		datei = open(os.path.join('data', 'thema.json'), 'r+')
+		thema_var = datei.read()
+		thema_var.update = thema
+		datei.seek()
+		datei.write(thema_var)
+		datei.close
+		
+	def lesen_themavz_db(self):
+		datei = open(os.path.join('data', 'thema.json'), 'r+')
+		thema = datei.read()
+		datei.close()
+		return thema	
+
+	def erstellen_diskussionvz_db(self, thema, diskussion, user):
+		pfad = path.join('data', thema)
+		mkdir(diskussion)
+		datei = open(os.path.join('data', thema, 'diskussion.json'), 'r+')
+		diskussion_var = datei.read()
+		diskussion_var.update = diskussion
+		datei.seek()
+		datei.write(diskussion_var)
+		datei.close
+			
+	def lesen_diskussionvz_db(self, thema):
+		datei = open(os.path.join('data', thema, 'thema.json'), 'r+')
+		themen = datei.read()
+		datei.close()
+		return diskussion
+## --------------------------------------------------------------------##
+
+
+
+## --------------------------------------------------------------------##
+## Diskussionen
+## --------------------------------------------------------------------##
+	def erstellen_diskussion_db(self, thema, diskussion, titel, inhalt):
+		# diskussion.json:
+		#	vorhandene .json beiträge (id)
+		#	username
+		#	Bearbeiter
+		#	zeitpunkt
+		#	titel
+		#	inhalt
+	
+	def lesen_diskussion_db(self, thema, diskussion):
+		# diskussion.json auslesen:
+		#	welche dateien müssen gelesen werden
+		#	erster post
+		# weitere posts .json dateien auslesen
